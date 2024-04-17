77bff725c3d56a02a8b5aa6c856e545a2f362f93
Dmitriy Grey <dmitriyiks@vk.com>
2024-04-17 10:25:09 +0300

    Update pseudorandomKey.py

--- pseudorandomKey.py
+++ pseudorandomKey.py
@@ -1,50 +1,114 @@
-#!usr/bin/python
+# randomizing algorithm
 
-#entropy with python , using mouse moves
+import time
+import math
+
+minnum = int(input('Enter your min value > '))
+maxnum = int(input('Enter your max value > '))
+numsum = minnum
+
+numstring = ''
+numlist = []
+i = minnum
+
+while i <= maxnum:
+    numlist.append(i)
+    i += 1
+
+def lengthoflist(thislist):
+    numsinlist = 0
+    while True:
+        try:
+            blank = thislist[numsinlist]
+
+        except:
+            break
+
+        numsinlist += 1
+
+    return numsinlist
+
+def shuffle_list(oldlist):
+    num = -1
+    newlist = []
+
+    while num < lengthoflist(oldlist) - 1:
+        newlist.append(oldlist[num])
+        num += 1
+
+    return newlist
 
 
-import pyautogui
-import hashlib
-import random
+def randomize_list(numlist):
+    global i, numtoadd
 
-#sceern size
-width, height = pyautogui.size()
-print width,height
-#move the mouse
-def makeKey(position):
-	position = set(position)
-	readyInput = ''.join(position)
-	firstPass = hashlib.sha256(readyInput.encode()).hexdigest()
-	secondPass = hashlib.sha256(firstPass.encode()).hexdigest()
-	print secondPass
+    numsinlist = lengthoflist(numlist)
+
+    randomnum = ' '
+
+    counter = 0
+
+    if i >= numsinlist:
+        while i >= numsinlist:
+            i = i - (maxnum - minnum)
+
+    if numtoadd >= numsinlist:
+        numtoadd = numtoadd - numsinlist
+
+    randomnum = numlist[i]
+
+    i += numtoadd
+
+    numtoadd += 1
+
+    counter += 1
+
+    return randomnum
 
 
-def randomKey():
-	#mouse move by its own generating random numbers
-	position1=[]
-	inputA = int(raw_input('Give a number the bigger the number the bigger the entropy and so as the security: '))
-	for i in range(inputA):
-		x = random.randint(1,inputA**2)
-		y = random.randint(1,inputA**2)
-		#mouse moves based on random numbers
-		pyautogui.moveRel(x, y, duration=0)
-		x,y = pyautogui.position()
-		position1.append(str(x))
-		position1.append(str(y))
-	makeKey(position1)
+j = 0
 
-def makeYourKey():
-	#move the mouse by your own
-	#make antropy and generate random key
-	position=[]
-	print 'Move your mouse...'
-	try:
-		while(True):
-			#take all the mouse's moves
-			x,y = pyautogui.position()
-			position.append(str(x))
-			position.append(str(y))
-	except KeyboardInterrupt:
-		print('\nDone')
-	#make the list unique
-	makeKey(position)
+numtoadd = 44
+
+numscalled = ' '
+
+numlist = shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(numlist)))))))
+
+counter = 0
+
+countedlist = ' '
+
+numscalled = 0
+
+totalnums = 0
+
+while True:
+    blah = randomize_list(numlist)
+
+    totalnums += 1
+
+    if not ' ' + str(blah) + ' ' in countedlist:
+        countedlist = countedlist + ' ' + str(blah) + ' '
+
+        numscalled += 1
+
+    print('Random number ' + str(blah))
+
+    print(countedlist)
+
+    print(' ', numscalled, ' ', totalnums)
+
+    time.sleep(0.02)
+
+    if numscalled >= maxnum - minnum + 1:
+        break
+
+    if counter >= 5:
+        numlist = shuffle_list(
+            shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(shuffle_list(numlist))))))))
+        counter = 0
+
+    counter += 1
+    j += 1
+
+print('Took ', totalnums, ' tries')
